a02e1081b2194b0f6f17b109c6b0bc0150a406ec
Jonathan Wang <jonathanpwang@gmail.com>
2022-08-03 12:00:46 -0700

    ecc: update MSM to work for arbitrary CurveAffine trait

--- src/ecc/mod.rs
+++ src/ecc/mod.rs
@@ -1,11 +1,10 @@
 #![allow(non_snake_case)]
 use std::marker::PhantomData;
 
+use group::Group;
 use halo2_proofs::{
-    arithmetic::{BaseExt, Field, FieldExt},
-    circuit::*,
-    pairing::bn256::{Fq as Fp, Fr, G1Affine},
-    pairing::group::ff::PrimeField,
+    arithmetic::{BaseExt, CurveAffine, Field, FieldExt},
+    circuit::{AssignedCell, Layouter},
     plonk::{Advice, Column, ConstraintSystem, Error, Fixed},
 };
 use num_bigint::{BigInt, BigUint};
@@ -28,6 +27,9 @@ use crate::utils::{
     bigint_to_fe, decompose_bigint_option, decompose_biguint, fe_to_bigint, fe_to_biguint, modulus,
 };
 
+pub mod fixed;
+use fixed::{fixed_base_scalar_multiply, FixedEccPoint};
+
 // EccPoint and EccChip take in a generic `FieldChip` to implement generic elliptic curve operations on arbitrary field extensions (provided chip exists) for short Weierstrass curves (currently further assuming a4 = 0 for optimization purposes)
 #[derive(Debug)]
 pub struct EccPoint<F: FieldExt, FC: FieldChip<F>> {
@@ -47,35 +49,6 @@ impl<F: FieldExt, FC: FieldChip<F>> Clone for EccPoint<F, FC> {
 impl<F: FieldExt, FC: FieldChip<F>> EccPoint<F, FC> {
     pub fn construct(x: FC::FieldPoint, y: FC::FieldPoint) -> Self {
         Self { x, y }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct FixedEccPoint<F: FieldExt> {
-    x: FixedCRTInteger<F>,
-    y: FixedCRTInteger<F>,
-}
-
-impl<F: FieldExt> FixedEccPoint<F> {
-    pub fn construct(x: FixedCRTInteger<F>, y: FixedCRTInteger<F>) -> Self {
-        Self { x, y }
-    }
-
-    pub fn from_g1(P: &G1Affine, num_limbs: usize, limb_bits: usize) -> Self {
-        let x_pt = FixedCRTInteger::from_native(fe_to_bigint(&P.x), num_limbs, limb_bits);
-        let y_pt = FixedCRTInteger::from_native(fe_to_bigint(&P.y), num_limbs, limb_bits);
-        Self { x: x_pt, y: y_pt }
-    }
-
-    pub fn assign(
-        &self,
-        chip: &FpChip<F>,
-        layouter: &mut impl Layouter<F>,
-    ) -> Result<EccPoint<F, FpChip<F>>, Error> {
-        let assigned_x = self.x.assign(&chip.config.range.qap_config, layouter)?;
-        let assigned_y = self.y.assign(&chip.config.range.qap_config, layouter)?;
-        let point = EccPoint::construct(assigned_x, assigned_y);
-        Ok(point)
     }
 }
 
@@ -370,148 +343,21 @@ where
     Ok(curr_point.clone())
 }
 
-// computes x * P on y^2 = x^3 + b
-// assumes:
-//   * 0 < x < scalar field modulus
-//   * P has order given by the scalar field modulus
-pub fn fixed_base_scalar_multiply<F: FieldExt>(
-    chip: &FpChip<F>,
-    layouter: &mut impl Layouter<F>,
-    P: &FixedEccPoint<F>,
-    x: &AssignedCell<F, F>,
-    b: F,
-    max_bits: usize,
-    window_bits: usize,
-) -> Result<EccPoint<F, FpChip<F>>, Error> {
-    let num_windows = (max_bits + window_bits - 1) / window_bits;
-    let rounded_bitlen = num_windows * window_bits;
-
-    // cached_points[i][j] holds j * 2^(i * w) for j in {0, ..., 2^w - 1}
-    let mut cached_points = Vec::with_capacity(num_windows);
-    let mut base_pt = G1Affine::default();
-    base_pt.x = bigint_to_fe(&P.x.value);
-    base_pt.y = bigint_to_fe(&P.y.value);
-    let base_pt_assigned = P.assign(&chip, layouter)?;
-    let mut increment = base_pt;
-    for i in 0..num_windows {
-        let mut cache_vec = Vec::with_capacity(1usize << window_bits);
-        let mut curr = increment;
-
-        let increment_fixed = FixedEccPoint::from_g1(
-            &increment,
-            P.x.truncation.limbs.len(),
-            P.x.truncation.limb_bits,
-        );
-        let increment_assigned = increment_fixed.assign(&chip, layouter)?;
-        cache_vec.push(increment_assigned.clone());
-        cache_vec.push(increment_assigned.clone());
-        for j in 2..(1usize << window_bits) {
-            curr = G1Affine::from(curr + increment);
-            let curr_fixed =
-                FixedEccPoint::from_g1(&curr, P.x.truncation.limbs.len(), P.x.truncation.limb_bits);
-            let curr_assigned = curr_fixed.assign(&chip, layouter)?;
-            cache_vec.push(curr_assigned);
-        }
-        increment = G1Affine::from(curr + increment);
-        cached_points.push(cache_vec);
-    }
-
-    let mut rounded_bits = Vec::with_capacity(rounded_bitlen);
-    let bits = chip.config.range.num_to_bits(layouter, x, max_bits)?;
-    for cell in bits.iter() {
-        rounded_bits.push(cell.clone());
-    }
-    let zero_cell = layouter.assign_region(
-        || "constant 0",
-        |mut region| {
-            let zero_cells = vec![Constant(F::from(0))];
-            let zero_cells_assigned =
-                chip.config
-                    .range
-                    .qap_config
-                    .assign_region(zero_cells, 0, &mut region)?;
-            Ok(zero_cells_assigned[0].clone())
-        },
-    )?;
-    for idx in 0..(rounded_bitlen - max_bits) {
-        rounded_bits.push(zero_cell.clone());
-    }
-
-    // is_started[idx] holds whether there is a 1 in bits with index at least (rounded_bitlen - idx)
-    let mut is_started = Vec::with_capacity(rounded_bitlen);
-    is_started.push(zero_cell.clone());
-    for idx in 1..rounded_bitlen {
-        let or = chip.config.range.qap_config.or(
-            layouter,
-            &Existing(&is_started[idx - 1]),
-            &Existing(&rounded_bits[rounded_bitlen - idx]),
-        )?;
-        is_started.push(or.clone());
-    }
-
-    // is_zero_window[idx] is 0/1 depending on whether bits [rounded_bitlen - window_bits * (idx + 1), rounded_bitlen - window_bits * idx) are all 0
-    let mut is_zero_window = Vec::with_capacity(num_windows);
-    let mut ones_vec = Vec::with_capacity(window_bits);
-    for idx in 0..window_bits {
-        ones_vec.push(Constant(F::from(1)));
-    }
-    for idx in 0..num_windows {
-        let temp_bits = rounded_bits
-            [rounded_bitlen - window_bits * (idx + 1)..rounded_bitlen - window_bits * idx]
-            .iter()
-            .map(|x| Existing(&x))
-            .collect();
-        let bit_sum = chip
-            .config
-            .range
-            .qap_config
-            .inner_product(layouter, &ones_vec, &temp_bits)?;
-        let is_zero = chip.config.range.is_zero(layouter, &bit_sum.2)?;
-        is_zero_window.push(is_zero.clone());
-    }
-
-    // if all the starting window bits are 0, get start_point = P
-    let mut curr_point = select_from_bits(
-        chip,
-        &chip.config.range,
-        layouter,
-        &cached_points[num_windows - 1],
-        &rounded_bits[rounded_bitlen - window_bits..rounded_bitlen].to_vec(),
-    )?;
-    for idx in 1..num_windows {
-        let add_point = select_from_bits(
-            chip,
-            &chip.config.range,
-            layouter,
-            &cached_points[num_windows - idx - 1],
-            &rounded_bits
-                [rounded_bitlen - window_bits * (idx + 1)..rounded_bitlen - window_bits * idx]
-                .to_vec(),
-        )?;
-        let sum = ecc_add_unequal(chip, layouter, &curr_point, &add_point)?;
-        let zero_sum = select(chip, layouter, &curr_point, &sum, &is_zero_window[idx])?;
-        curr_point = select(
-            chip,
-            layouter,
-            &zero_sum,
-            &add_point,
-            &is_started[window_bits * idx],
-        )?;
-    }
-    Ok(curr_point.clone())
-}
-
-// only works for FpChip for now because it needs to generate a random G1Affine point and we don't need to generalize this at the moment
-pub fn multi_scalar_multiply<F: FieldExt>(
-    chip: &FpChip<F>,
+// need to supply an extra generic `GA` implementing `CurveAffine` trait in order to generate random witness points on the curve in question
+pub fn multi_scalar_multiply<F: FieldExt, FC, GA>(
+    chip: &FC,
     range: &range::RangeConfig<F>,
     layouter: &mut impl Layouter<F>,
-    P: &Vec<EccPoint<F, FpChip<F>>>,
+    P: &Vec<EccPoint<F, FC>>,
     x: &Vec<AssignedCell<F, F>>,
     b: F,
     max_bits: usize,
     window_bits: usize,
-) -> Result<EccPoint<F, FpChip<F>>, Error> {
+) -> Result<EccPoint<F, FC>, Error>
+where
+    FC: FieldChip<F>,
+    GA: CurveAffine<Base = FC::FieldType>,
+{
     let k = P.len();
     assert_eq!(k, x.len());
     let num_windows = (max_bits + window_bits - 1) / window_bits;
@@ -560,13 +406,13 @@ pub fn multi_scalar_multiply<F: FieldExt>(
         is_zero_window_vec.push(is_zero_window);
     }
 
-    let base_point = G1Affine::random(OsRng);
-    let pt_x = fe_to_bigint(&base_point.x);
-    let pt_y = fe_to_bigint(&base_point.y);
+    let base_point = GA::random(OsRng());
+    let base_point_coord = base_point.coordinates().unwrap();
+    let pt_x = FC::fe_to_witness(&Some(base_point_coord.x()));
+    let pt_y = FC::fe_to_witness(&Some(base_point_coord.y()));
     let base = {
-        let x_overflow = chip.load_private(layouter, Some(pt_x))?;
-        let y_overflow = chip.load_private(layouter, Some(pt_y))?;
-
+        let x_overflow = chip.load_private(layouter, pt_x)?;
+        let y_overflow = chip.load_private(layouter, pt_y)?;
         EccPoint::construct(x_overflow, y_overflow)
     };
 
@@ -650,15 +496,7 @@ impl<F: FieldExt, FC: FieldChip<F>> EccChip<F, FC> {
     where
         FC::FieldType: BaseExt,
     {
-        FpChip::configure(
-            meta,
-            value,
-            constant,
-            lookup_bits,
-            limb_bits,
-            num_limbs,
-            BigUint::from_str_radix(&FC::FieldType::MODULUS[2..], 16).unwrap(),
-        )
+        FpChip::configure(meta, value, constant, lookup_bits, limb_bits, num_limbs)
     }
 
     pub fn load_private(
@@ -727,18 +565,19 @@ impl<F: FieldExt, FC: FieldChip<F>> EccChip<F, FC> {
             window_bits,
         )
     }
-}
 
-impl<F: FieldExt> EccChip<F, FpChip<F>> {
-    pub fn multi_scalar_mult(
+    pub fn multi_scalar_mult<GA>(
         &self,
         layouter: &mut impl Layouter<F>,
-        P: &Vec<EccPoint<F, FpChip<F>>>,
+        P: &Vec<EccPoint<F, FC>>,
         x: &Vec<AssignedCell<F, F>>,
         b: F,
         max_bits: usize,
         window_bits: usize,
-    ) -> Result<EccPoint<F, FpChip<F>>, Error> {
+    ) -> Result<EccPoint<F, FC>, Error>
+    where
+        GA: CurveAffine<Base = FC::FieldType>,
+    {
         multi_scalar_multiply(
             &self.field_chip,
             &self.field_chip.config.range,
@@ -750,16 +589,18 @@ impl<F: FieldExt> EccChip<F, FpChip<F>> {
             window_bits,
         )
     }
+}
 
+impl<F: FieldExt, GA: CurveAffine> EccChip<F, FpChip<F, GA::Base>> {
     pub fn fixed_base_scalar_mult(
         &self,
         layouter: &mut impl Layouter<F>,
-        P: &FixedEccPoint<F>,
+        P: &FixedEccPoint<F, GA>,
         x: &AssignedCell<F, F>,
         b: F,
         max_bits: usize,
         window_bits: usize,
-    ) -> Result<EccPoint<F, FpChip<F>>, Error> {
+    ) -> Result<EccPoint<F, FpChip<F, GA::Base>>, Error> {
         fixed_base_scalar_multiply(&self.field_chip, layouter, P, x, b, max_bits, window_bits)
     }
 }
