1e4c7ff0a0a5ae204c6d4ac55034e398580787fb
Zhang Zhuo <mycinbrin@gmail.com>
2023-09-04 09:49:40 +0800

    clippy

--- halo2-ecc/src/lib.rs
+++ halo2-ecc/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::op_ref)]
 #![allow(clippy::type_complexity)]
+#![allow(unused_imports)]
+#![allow(stable_features)]
+#![feature(int_log)]
 #![feature(trait_alias)]
 
 pub mod bigint;
