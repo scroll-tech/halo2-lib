6d982760759410c311d1c5e3c01c8451f0651d40
Dream Wu <wwuwwei@126.com>
2024-07-09 17:42:57 +0800

    fix field_chip.mul

--- halo2-ecc/src/ecc/mod.rs
+++ halo2-ecc/src/ecc/mod.rs
@@ -343,14 +343,15 @@ where
     // calculate x^3
     rhs = chip.mul_no_carry(ctx, &rhs, &P.x);
 
-    let a = FC::fe_to_constant(C::a());
+    let a_const = FC::fe_to_constant(C::a());
+    let a = chip.load_constant(ctx, a_const);
     let b = FC::fe_to_constant(C::b());
     // calculate x^3 + b
     rhs = chip.add_constant_no_carry(ctx, &rhs, b);
     // calculate `a*x` part of the whole equation: `y^2= x^3 + a*x + b`, if a = 0 (for secp256k1),
     // `a*x` is zero.
     // add chip.mul_constant helper ?
-    let ax = chip.mul(ctx, &P.x, a.into());
+    let ax = chip.mul(ctx, &P.x, &a);
     // calculate x^3 + a*x  + b
     rhs = chip.add_no_carry(ctx, &rhs, &ax);
 
@@ -627,13 +628,15 @@ impl<F: PrimeField, FC: FieldChip<F>> EccChip<F, FC> {
         let mut rhs = self.field_chip.mul(ctx, &P.x, &P.x);
         rhs = self.field_chip.mul_no_carry(ctx, &rhs, &P.x);
 
-        let a = FC::fe_to_constant(C::a());
+        let a_const = FC::fe_to_constant(C::a());
+        let a = self.field_chip.load_constant(ctx, a_const);
+
         let b = FC::fe_to_constant(C::b());
         rhs = self.field_chip.add_constant_no_carry(ctx, &rhs, b);
         // calculate `a*x` part of the whole equation: `y^2= x^3 + a*x + b`, if a = 0 (for secp256k1),
         // `a*x` is zero.
         // add chip.mul_constant helper ?
-        let ax = self.field_chip.mul(ctx, &P.x, a.into());
+        let ax = self.field_chip.mul(ctx, &P.x, &a);
         // calculate x^3 + a*x  + b
         rhs = self.field_chip.add_no_carry(ctx, &rhs, &ax);
         let mut diff = self.field_chip.sub_no_carry(ctx, &lhs, &rhs);
